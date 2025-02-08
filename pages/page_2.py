1761b9e324b6d8526683af35fc700f63f3d29c25
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 14:56:31 -0500

    d

--- pages/page_2.py
+++ pages/page_2.py
@@ -5,6 +5,7 @@ st.title("Recycling")
 st.header("Want to know if you can recycle something?")
 with st.container(border=True):
     st.write("hello?")
+    st.page_link("recycling/materials.py", label="Recycle?")
     #st.page_link("pages/recycling/materials.py", label="Recycle?", use_container_width=True)
     #st.page_link("Poorv-Lal/FemmHacks_2025/pages/recycling/materials.py", label="TAKE ACTION")
 

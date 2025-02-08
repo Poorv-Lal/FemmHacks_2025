5de265aac789c3c6ab624e21813d7b74937aa3de
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 11:47:21 -0500

    update

--- main.py
+++ main.py
@@ -2,7 +2,12 @@
 
 import streamlit as st
 
-pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py"),st.Page("page_3.py"),st.Page("page_4.py"),st.Page("page_5.py"),st.Page("page_6.py")])
+pg = st.navigation([st.Page("page_1.py",title="Homepage"),
+                     st.Page("page_2.py",title="Recycling"),
+                     st.Page("page_3.py",title="Clothing"),
+                     st.Page("page_4.py",title="Composting"),
+                     st.Page("page_5.py",title="Microplastics Avoiding"),
+                     st.Page("page_6.py",title="Call to Action")])
 pg.run()
     
 

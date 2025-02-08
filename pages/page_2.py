a363008914bcf763f17f260b15ebd5c54e393c34
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 15:17:31 -0500

    m

--- pages/page_2.py
+++ pages/page_2.py
@@ -3,14 +3,3 @@ import streamlit as st
 st.title("Recycling")
 
 st.header("Want to know if you can recycle something?")
-with st.container(border=True):
-    st.write("hello?")
-    st.page_link("pages/materials.py", label="TAKE ACTION", icon="📢")
-
-    #st.page_link("pages/recycling/materials.py", label="Recycle?", use_container_width=True)
-    #st.page_link("Poorv-Lal/FemmHacks_2025/pages/recycling/materials.py", label="TAKE ACTION")
-
-#st.divider()
-#st.header("Take a Quiz to Test your Recycle Knowledge?")
-#with st.container(border=True):
-#    st.page_link("pages/recycling/quiz.py", label="Start Quiz", use_container_width=True)

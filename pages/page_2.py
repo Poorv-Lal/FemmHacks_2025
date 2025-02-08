aa5fba76b808ada1a266a06a15b9fbfe48ccc96f
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 16:04:23 -0500

    a

--- pages/page_2.py
+++ pages/page_2.py
@@ -10,19 +10,28 @@ st.header("Want to know if you can recycle something?")
 option = st.selectbox(
     "What kind of Material do you have?",
     ("Carton", "Paper", "Cardboard","Food Scraps", "Glass","Plastic","Metal","Styrofoam", "Other/Unsure"),
-)
+    )
 
 if option == "Paper":
     option2 = st.selectbox(
-    "What kind of Material do you have?",
+    "Is the " + option +" ______?",
     ("Clean & Dry","Wet", "Shredded/In-small Pieces"),
-)
+    )
+
+    if option2 == "Clean & Dry":
+        can_recycle = 1
     
-if option == "Carton":
+elif option == "Carton":
     option2 = st.selectbox(
-    "What kind of Material do you have?",
+    "Is the " + option +" ______?",
     ("Clean & Dry","Has Food On it", "Shredded/In-small Pieces"),
-)
+    )
+
+elif option == "Cardboard":
+    option2 = st.selectbox(
+    "Is the " + option +" ______?",
+    ("Clean & Dry","Have Food On it", "Shredded/In-small Pieces"),
+    )
     
 if can_recycle == 1:
     st.success("You can Recycle this!!! ♻️")

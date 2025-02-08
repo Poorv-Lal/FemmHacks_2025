b88f092062b72a47fe80c1bc50276febde4573f9
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 09:33:48 -0500

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+#Name: Ada Anya, Abbey Ham, Poorv Lal
+
+import streamlit as st 
+
+# HIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+# hey!
+if __name__ == "__main__":
+    st.write("Hello World!")
+
+
+
+
+
+

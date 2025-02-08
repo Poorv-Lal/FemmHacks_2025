7c2080b0701dd86b7cc9ada098cb941ee2ac143a
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 10:59:12 -0500

    add pages

--- main.py
+++ main.py
@@ -2,10 +2,7 @@
 
 import streamlit as st
 
-def page_2():
-    st.title("Page 2")
-
-pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
+pg = st.navigation([st.Page("page_1.py"), st.Page("page_1.py"),])
 pg.run()
     
 

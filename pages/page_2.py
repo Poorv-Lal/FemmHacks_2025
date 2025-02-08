6349dcff611a0935b686a90904014c1bee0e97f8
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 14:47:46 -0500

    abbey

--- pages/page_2.py
+++ pages/page_2.py
@@ -4,9 +4,9 @@ st.title("Recycling")
 
 st.header("Want to know if you can recycle something?")
 with st.container(border=True):
-    st.page_link("pages/recycling/materials", label="Recycle?", use_container_width=True)
+    st.page_link("pages/recycling/materials.py", label="Recycle?", use_container_width=True)
 
 st.divider()
 st.header("Take a Quiz to Test your Recycle Knowledge?")
 with st.container(border=True):
-    st.page_link("pages/recycling/quiz", label="Start Quiz", use_container_width=True)
+    st.page_link("pages/recycling/quiz.py", label="Start Quiz", use_container_width=True)

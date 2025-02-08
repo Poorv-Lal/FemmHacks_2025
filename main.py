51c8487b3deeec3cc1f7e3dbd8ee7fecd5b72d1f
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 11:42:55 -0500

    update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import streamlit as st
 
-
 pg = st.navigation([st.Page("page_1.py"), st.Page("page_2.py"),st.Page("page_3.py"),st.Page("page_4.py"),st.Page("page_5.py"),st.Page("page_6.py")])
 pg.run()
     

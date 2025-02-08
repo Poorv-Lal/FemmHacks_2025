fdcd445d788ebe1140e036415acbf389e8a59856
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 13:25:49 -0500

    fdb

--- pages/page_1.py
+++ pages/page_1.py
@@ -26,7 +26,7 @@ with col5:
 
 
 st.divider()
-with st.container():
+with st.container(border=True):
     st.header("Our mission - SPROUT")
     st.subheader("Sustainability")
     st.write("Taking care of the earth in small ways where you can while meeting our individual and communal needs")

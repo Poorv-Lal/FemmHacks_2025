eb3534a52a7311b5796f967efdd65fadc2990c79
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 15:09:20 -0500

    m

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 import streamlit as st
 
+st.Page("pages/materials.py",title="Materials")
+
 pg = st.navigation([st.Page("pages/page_1.py",title="Homepage"),
                      st.Page("pages/page_2.py",title="Recycling"),
                      st.Page("pages/page_3.py",title="Clothing"),

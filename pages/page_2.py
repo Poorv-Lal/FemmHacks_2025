68fb6e3f86728bf0b6da49546885581a8ee0ef9a
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 15:35:43 -0500

    a

--- pages/page_2.py
+++ pages/page_2.py
@@ -2,4 +2,22 @@ import streamlit as st
 
 st.title("Recycling")
 
+can_recycle = False
+
+
 st.header("Want to know if you can recycle something?")
+option = st.selectbox(
+    "What kind of Material do you have?",
+    ("Carton", "Paper", "Cardboard","Food Scraps", "Glass","Plastic","Metal","Styrofoam", "Other/Unsure"),
+)
+
+if option == "Carton":
+    option2 = st.selectbox(
+    "What kind of Material do you have?",
+    ("Dry&Clean","Has Food On it", "Shredded/In-small Pieces"),
+)
+    
+if can_recycle == 1:
+    st.success("You can Recycle this!!!")
+else:
+    st.error("You can NOT Recycled this!!!")

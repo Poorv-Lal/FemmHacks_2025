fe7713233e6695fba521c4baad6783a4d7355b5b
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 13:35:59 -0500

    abbey

--- pages/page_2.py
+++ pages/page_2.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.title("Recycling")
+
+optionMaterial = st.selectbox(
+    "What material are you tring to recycle?",
+    ("Paper", "Carboard", "Glass", "Plastic","Metal","Batteries/Electronics", "Mixed Material", "Stryofoam", "Other", "Unsure"),
+)
+
+st.write("You selected:", optionMaterial)

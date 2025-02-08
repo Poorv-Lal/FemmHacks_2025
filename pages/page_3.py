95d5043d4882a9577b1f1acf7de3cf9df429b370
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 15:15:43 -0500

    poiu

--- pages/page_3.py
+++ pages/page_3.py
@@ -3,4 +3,4 @@ import streamlit as st
 #st.image("FemmHacks_2025/Images/clothing_header.jpg")
 st.title("What's Your Style?")
 st.write("Take this quiz to know what fashion sustainability practice suits you!")
-st.write("hello?")
+st.page_link("pages/page_2.py", label="Start Quiz")

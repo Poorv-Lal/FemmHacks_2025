f9304e5182c8072d200170ebe3fdbcb15432be79
Poorv-Lal <PoorvBLal@gmail.com>
2025-02-08 11:54:26 -0500

    reorgainse

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 import streamlit as st
 
 pg = st.navigation([st.Page("pages/page_1.py",title="Homepage"),
-                     st.Page("page_2.py",title="Recycling"),
-                     st.Page("page_3.py",title="Clothing"),
-                     st.Page("page_4.py",title="Composting"),
-                     st.Page("page_5.py",title="Microplastics Avoiding"),
-                     st.Page("page_6.py",title="Call to Action")])
+                     st.Page("pages/page_2.py",title="Recycling"),
+                     st.Page("pages/page_3.py",title="Clothing"),
+                     st.Page("pages/page_4.py",title="Composting"),
+                     st.Page("pages/page_5.py",title="Microplastics Avoiding"),
+                     st.Page("pages/page_6.py",title="Call to Action")])
 pg.run()
     
 
